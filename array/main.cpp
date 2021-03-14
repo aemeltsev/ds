c76f169a79beed35c6041a1ce04b409812a11d37
Tony Emeltsev <aemeltsev@gmail.com>
2021-03-14 10:33:44 +0300

    clear main source

--- array/main.cpp
+++ array/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-
-using namespace std;
+#include "array.h"
 
 int main()
 {
-    cout << "Hello World!" << endl;
     return 0;
 }
--- deque/main.cpp
+++ deque/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include "deque.h"
 
-using namespace std;
-
 int main()
 {
-    cout << "Hello World!" << endl;
     return 0;
 }
