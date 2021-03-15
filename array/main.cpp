09c3f1caa3a0ed4ac8c3d2c3d6f8c00f025d0709
Tony Emeltsev <aemeltsev@gmail.com>
2021-03-15 17:33:48 +0300

    test: add test heder to main, preconfig tests.h

--- array/main.cpp
+++ array/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "array.h"
+#include "tests.h"
 
 int main()
 {
--- array/tests/tests.h
+++ array/tests/tests.h
@@ -1,1 +1,13 @@
+#ifndef ARRAY_TESTS_H
+#define ARRAY_TESTS_H
 
+#include <ctime>
+#include <gtest/gtest.h>
+
+#include "array.h"
+
+namespace array_testing {
+
+}
+
+#endif //ARRAY_TESTS_H
