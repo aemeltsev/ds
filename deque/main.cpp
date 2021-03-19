b6f1269bab4a9fa582a3cd15801bb6f83416cb69
Tony Emeltsev <aemeltsev@gmail.com>
2021-03-19 13:03:13 +0300

    test: configure main.cpp, tests.h

--- deque/main.cpp
+++ deque/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include "deque.h"
+#include "tests.h"
 
-int main()
+int main(int argc, char **argv)
 {
-    return 0;
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }
---
+++ deque/tests/tests.h
@@ -0,0 +1,16 @@
+#ifndef DEQUE_TESTS_H
+#define DEQUE_TESTS_H
+
+#include <iostream>
+#include <ctime>
+#include <gtest/gtest.h>
+
+#include "deque.h"
+
+namespace deque_testing {
+
+
+
+}
+
+#endif //DEQUE_TESTS_H
